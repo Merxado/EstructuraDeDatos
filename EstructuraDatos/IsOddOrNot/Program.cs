32805caeadcc4a815c098ab062683b985c5ad153
Merxado <mayimamiyyo1112@gmail.com>
2026-02-10 18:18:01 -0500

    Complete exercice 01

---
+++ EstructuraDatos/IsOddOrNot/Program.cs
@@ -0,0 +1,28 @@
+﻿var numberString = String.Empty;
+do
+{
+    Console.Write("Ingrese un numero o la palabra 'Salir' para finalizar: ");
+    numberString = Console.ReadLine();
+    if (numberString!.ToLower() == "salir")
+    {
+        Console.WriteLine("Saliendo del programa...");
+        break;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"El numero {numberString} es par");
+        }
+        else
+        {
+            Console.WriteLine("El numero es impar");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El valor {numberString}, ingresado no es un numero valido");
+    }
+} while (numberString.ToLower() != "salir");
+
